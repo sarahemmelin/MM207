a0804902ca584e3cb72f5a5bf96c142e3de824e7
Sarah Emmelin Johansen Schanke <sarahemmelin@gmail.com>
2025-01-08 17:45:39 +0100

    endringer

--- nodetutorial/myfirst.js
+++ nodetutorial/myfirst.js
@@ -1,9 +1,20 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+// server.listen(8080, () => {
+//   console.log('Server running at http://localhost:8080/');
+// });
+
+let http = require('http');
+
+let server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(8080);
+});
 
 server.listen(8080, () => {
   console.log('Server running at http://localhost:8080/');
