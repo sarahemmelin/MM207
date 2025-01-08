25785e74de11cd6531eee6533dc2486b8b64ffa8
Sarah Emmelin Johansen Schanke <sarahemmelin@gmail.com>
2025-01-08 17:56:45 +0100

    Tilbakemelding i terminalen

--- nodetutorial/myfirst.js
+++ nodetutorial/myfirst.js
@@ -1,14 +1,3 @@
-// var http = require('http');
-
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.end('Hello World!');
-// }).listen(8080);
-
-// server.listen(8080, () => {
-//   console.log('Server running at http://localhost:8080/');
-// });
-
 let http = require('http');
 
 let server = http.createServer(function (req, res) {
